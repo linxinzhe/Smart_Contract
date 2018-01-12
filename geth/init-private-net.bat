f7cf615cc5b61145d05250f484e114194e9b0925
linxinzhe <linxinzhechn@icloud.com>
2018-01-12 10:37:57 +0800

    feat(geth): init private network and start private network

---
+++ geth/init-private-net.bat
@@ -0,0 +1,3 @@
+geth --identity "etherdev1" --rpc --rpcport "8080" --rpccorsdomain "*" --datadir "E:\Users\linxinzhe\ethereum\etherdev1" --port "30303" --nodiscover --rpcapi "db,eth,net,web3" --networkid 1999 init "./CustomGenesis.json"
+
+echo press any & pause
