b5fe8c4de81ff4f530d708df442abbe79c45721d
linxinzhe <linxinzhechn@icloud.com>
2018-01-13 11:00:16 +0800

    refactor(geth): blockchain data path

--- geth/start-private-net.bat
+++ geth/start-private-net.bat
@@ -1,2 +1,2 @@
-geth --identity "etherdev1" --rpc --rpcport "8080" --rpccorsdomain "*" --datadir "E:\Users\linxinzhe\ethereum\etherdev1" --port "30303" --nodiscover --rpcapi "db,eth,net,web3" --networkid 1999 console
+geth --identity "etherdev1" --rpc --rpcport "8080" --rpccorsdomain "*" --datadir "E:\ethereum\etherdev1" --port "30303" --nodiscover --rpcapi "db,eth,net,web3" --networkid 1999 console
 pause
